c2849e9c4e7d5a551dff1eb8416876211c426567
Joel Aumedes <joelaumedes@gmail.com>
2024-12-11 23:39:33 +0100

    Per allah les colisions funcionen

--- Assets/Scripts/LookAround.cs
+++ Assets/Scripts/LookAround.cs
@@ -1,25 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAround : MonoBehaviour {
+    [SerializeField] private float horizontalSensitivity = 800f;
+    [SerializeField] private float verticalSensitivity = 3.6f;
+
+    [SerializeField] private GameObject player;
+
+    private float rotationY = 0.0f;
     
-    [SerializeField]
-    private float sensitivity = 50000f;
-
-    [SerializeField]
-    private GameObject player;
-
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update() {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-        
-        transform.Rotate(-mouseY * sensitivity * Time.deltaTime * Vector3.right);
-        
-        player.transform.Rotate(mouseX * sensitivity * Time.deltaTime * Vector3.up);
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * verticalSensitivity;
+
+        rotationY -= mouseY;
+        rotationY = Mathf.Clamp(rotationY, -80, 80);
+
+        transform.localRotation = Quaternion.Euler(rotationY, 0, 0);
+
+        player.transform.Rotate(mouseX * Time.deltaTime * Vector3.up);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,35 @@ public class Player : MonoBehaviour {
 
     [SerializeField] private float speed = 3.0f;
     private float sprintMultiplier = 1.5f;
+
+    private CharacterController controller;
     
-    void Start() { }
+    private float gravityAcceleration = -9.81f;
+    private float verticalAcceleration = -1.0f;
+    
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update() {
         float hInput = Input.GetAxisRaw("Horizontal");
         float vInput = Input.GetAxisRaw("Vertical");
         
-        Vector3 movementDirection = new Vector3(hInput, 0, vInput).normalized;
+        bool grounded = controller.isGrounded;
+        
+        Vector3 movementDirectionLocal = new Vector3(hInput, 0, vInput).normalized;
+        Vector3 movementDirectionWorld = transform.TransformDirection(movementDirectionLocal);
         
         float totalSpeed = Input.GetKey(KeyCode.LeftShift) ? speed * sprintMultiplier : speed;
         
-        transform.Translate(totalSpeed * Time.deltaTime * movementDirection, Space.Self);
+        controller.Move(totalSpeed * Time.deltaTime * movementDirectionWorld);
+
+        verticalAcceleration += gravityAcceleration * Time.deltaTime;
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) {
+            verticalAcceleration = 5f;
+        }
+        
+        controller.Move(verticalAcceleration * Time.deltaTime * Vector3.up);
     }
     
     private void OnTriggerEnter(Collider other) {
